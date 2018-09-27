09f2c1256d636350e369548d3884660cc0c89acb
Abry Rath <abry.rath@union.co>
2018-09-27 09:01:08 -0400

    Updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,59 +6,83 @@ var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  entry: [
-    path.join(__dirname, '/assets/main.js'),
-  ],
-  output: {
-    path: path.join(__dirname, '/web'),
-    publicPath: '/',
-    filename: 'main.js'
-  },
-    mode: devMode ? 'development' : 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.jsx?$/,
-        include: path.join(__dirname, 'assets', 'js'),
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        include: path.join(__dirname, 'assets', 'scss'),
-        exclude: /node_modules/,
-        use: [
-            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i, 
-        loader: "file-loader?name=/public/icons/[name].[ext]"
-      }
-    ]
-  },
-  plugins: [
-      new VueLoaderPlugin(),
-      new MiniCssExtractPlugin({
-          filename: "[name].css",
-          chunkFilename: "[id].css"
-      })
-  ],
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+    entry: [
+        path.join(__dirname, '/assets/main.js'),
+    ],
+    output: {
+        path: path.join(__dirname, '/web'),
+        publicPath: '/',
+        //filename: 'main.js'
     },
-    extensions: ['*', '.js', '.vue', '.json']
-  },
-  serve: {
-    options: {
+    mode: devMode ? 'development' : 'production',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.jsx?$/,
+                include: path.join(__dirname, 'assets', 'js'),
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                include: path.join(__dirname, 'assets', 'scss'),
+                exclude: /node_modules/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '/',
+                        }
+                    },
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                loader: "file-loader?name=/public/icons/[name].[ext]"
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    serve: {
+        options: {
+        }
     }
-  }
 };
